ef5f533c9cb1a7e48adfa1da45e6bc56decdf22d
sivadharshini-embermuse <sivadharshinigithub17@gmail.com>
2026-03-01 15:47:47 +0530

    changes in cjheckpassword file

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const db=require('./config/db')
 // const attendanceRoutes = require('./routes/attendance');
 
 const app = express();
-app.use(cors({ origin: "https://aiml-lab-attendence-1.onrender.com" }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const routes = require("./routes/attendanceRoutes");
